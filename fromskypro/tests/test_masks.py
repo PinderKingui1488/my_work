165934fbcf15496d34b6efc3dbc12b0082f756a4
Velikiy astmatick <ananimusazahar@gmail.com>
2024-05-16 14:10:06 +0300

    add tests

--- fromskypro/tests/test_masks.py
+++ fromskypro/tests/test_masks.py
@@ -1,8 +1,10 @@
 from fromskypro.src.masks import mask_card_number, mask_account_number
 
+
 def test_mask_card_number():
     assert mask_card_number("1234567812345678") == "1234 56 **** 5678"
     assert mask_card_number("9876543210987654") == "9876 54 **** 7654"
+
 
 def test_mask_account_number():
     assert mask_account_number("1234567890") == "7890"
